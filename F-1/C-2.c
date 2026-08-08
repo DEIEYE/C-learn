3a87497919489157bea820c43994add42d164756
DEIEYE <xbottosxbot@gmail.com>
2026-08-08 20:56:54 +0800

    commit2:operation

---
+++ F-1/C-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 1;
+    float b = 2;
+    float c = a+b;
+    printf("%f",c);
+    for (int i=0;i<5;i++){
+        printf("%d\n", i);
+    }
+    return 0;
+}
