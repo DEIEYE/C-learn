e8073a31bdc447327c0672fa595bc592185822db
DEIEYE <xbottosxbot@gmail.com>
2026-09-20 14:29:51 +0800

    commit-6:??

--- F-2/C-1.c
+++ F-2/C-1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main() {
-
+    int A[5];
+    for (int i=0;i<5;i++) {
+        if (A[i]>=0) {
+            scanf("%d",&A[i]);
+        }
+    }
+    int sum = 0;
+    for (int i=0;i<5;i++) {
+        sum += A[i];
+    }
+    printf("所有整数的和是%d",sum);
+    scanf("%d",&sum);
     return 0;
 }
